4d2e95ed5204cbc3ba4deccb8ecc5548adfcb5af
Oren <orengolan@gmail.com>
2016-07-21 23:46:03 -0700

    load csv into a slice

--- main.go
+++ main.go
@@ -5,7 +5,17 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
+
+type Pokemon struct {
+	Id             int
+	Name           string
+	Species_id     int
+	Height         int
+	BaseExperience int
+}
 
 func main() {
 	file, err := os.Open("./pokemon.csv")
@@ -14,10 +24,24 @@ func main() {
 	}
 	defer file.Close()
 
+	pokemon := []Pokemon{}
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		s := strings.Split(scanner.Text(), ",")
+		id, err := strconv.Atoi(s[0])
+		speciesId, err := strconv.Atoi(s[2])
+		height, err := strconv.Atoi(s[3])
+		baseExperiment, err := strconv.Atoi(s[4])
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		pokemon = append(pokemon, Pokemon{id, s[1], speciesId, height, baseExperiment})
 	}
+
+	fmt.Println(pokemon)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
